3e620e8fb2580b3e506f7ebb7ed62767dd80400b
igitforfun <126478060+igitforfun@users.noreply.github.com>
2025-02-08 13:04:29 +0800

    new changes to script

--- extracttxn.py
+++ extracttxn.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 import argparse
 
+
 def parser_init():
     parser = argparse.ArgumentParser(description= 'filter and calculate the matched transactions')
     parser.add_argument('-f', '--file', dest='input_file', help = 'excel file to be processed')
@@ -17,7 +18,12 @@ def main():
         print("------------------------------------------------------------------------")
         for i in range(len(exceldata['Unnamed: 2'])): #Run through description column
             if (i >= 9):
-                if ((re.search(str(args.regex_word), str(exceldata['Unnamed: 2'][i]), re.IGNORECASE)) or not (args.regex_word)):
+                if args.regex_word:
+                    if ((re.search(str(args.regex_word), str(exceldata['Unnamed: 2'][i]), re.IGNORECASE)) or not (args.regex_word)):
+                        print(f"\033[93m{exceldata['Unnamed: 1'][i]}\033[0m | {exceldata['Unnamed: 2'][i]} : \033[96m{exceldata['Unnamed: 6'][i]}\033[0m")
+                        print("------------------------------------------------------------------------")
+                        totalcost += float(exceldata['Unnamed: 6'][i])
+                else:
                     print(f"\033[93m{exceldata['Unnamed: 1'][i]}\033[0m | {exceldata['Unnamed: 2'][i]} : \033[96m{exceldata['Unnamed: 6'][i]}\033[0m")
                     print("------------------------------------------------------------------------")
                     totalcost += float(exceldata['Unnamed: 6'][i])
